ecbfbfbe62e178dd2dec34c15d468488a3356ba9
Russell Gordon <github@russellgordon.ca>
2021-08-13 08:16:53 -0400

    Initial Commit

---
+++ NotificationsAndDatePickers/NotificationsAndDatePickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsAndDatePickersApp.swift
+//  NotificationsAndDatePickers
+//
+//  Created by Russell Gordon on 2021-08-13.
+//
+
+import SwiftUI
+
+@main
+struct NotificationsAndDatePickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
