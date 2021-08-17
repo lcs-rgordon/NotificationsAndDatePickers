1ab6fd5b61e9a3f8f499fa2640302b4b9f0065c6
Russell Gordon <github@russellgordon.ca>
2021-08-17 16:11:21 -0400

    Added example combining date pickers and notifications.

--- NotificationsAndDatePickers/Views/CombinationView.swift
+++ NotificationsAndDatePickers/Views/CombinationView.swift
@@ -8,13 +8,88 @@
 import SwiftUI
 
 struct CombinationView: View {
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .long
+        return formatter
+    }()
+    
+    @State private var futureDateAndTime = Date()
+    
     var body: some View {
-        Text("CombinationView")
+        ScrollView {
+            
+            VStack(alignment: .leading) {
+                
+                Text("Here we allow the user to select some date and time in the future, and do some arithmetic to determine the number of seconds between the current date and time and that was selected. A notification is schedule based upon that difference.")
+                    .multilineTextAlignment(.leading)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        askNotificationPermission()
+                    }, label: {
+                        Text("Enable Local Notifications")
+                    })
+                        .buttonStyle(.borderedProminent)
+                        .padding(.bottom)
+                    
+                    Spacer()
+                    
+                }
+                
+                Section(title: "Notification Date/Time")
+                
+                Text("When would you like the notification to occur?")
+                
+                DatePicker(selection: $futureDateAndTime, in: Date()...) {
+                    Text("Select a date and time")
+                        .bold()
+                }
+                .datePickerStyle(CompactDatePickerStyle())
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                        let delta = futureDateAndTime.timeIntervalSince(Date())
+                        
+                        publishNotification(title: "Example",
+                                            subtitle: "Some subtitle",
+                                            body: "Body of notification",
+                                            timeUntil: delta)
+                        
+                    }, label: {
+                        
+                        Text("Publish Notification")
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .padding(.bottom)
+                    
+                    Spacer()
+                }
+                    
+                Text("**NOTE:** Notifications will only appear on the home or lock screen.")
+                
+                Spacer()
+            }
+            .padding()
+            
+        }
+        .navigationTitle("Combination")
+        
     }
 }
 
 struct CombinationView_Previews: PreviewProvider {
     static var previews: some View {
-        CombinationView()
+        NavigationView {
+            CombinationView()
+        }
     }
 }
